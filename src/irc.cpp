a99c5cc5070f2d639f8577845fe5a89bad13d0c3
root <root@syncro1.c.smart-quasar-124716.internal>
2016-03-13 18:02:31 +0000

    change irc room

--- src/irc.cpp
+++ src/irc.cpp
@@ -348,10 +348,8 @@ void ThreadIRCSeed2(void* parg)
             Send(hSocket, "JOIN #bitcoinTEST\r");
             Send(hSocket, "WHO #bitcoinTEST\r");
         } else {
-            // randomly join #bitcoin00-#bitcoin99
-            int channel_number = GetRandInt(100);
-            Send(hSocket, strprintf("JOIN #bitcoin%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #bitcoin%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #bananacoin\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #bananacoin\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
